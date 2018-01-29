d7513738786bfb702b18913f29719c89ca394367
Stephen Thurman <thurman024@gmail.com>
2018-01-29 21:12:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,16 @@
 class Dog
   @@all = []
+  attr_reader :name
   def initialize(name)
     @name = name
     @@all << self
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+
+end
